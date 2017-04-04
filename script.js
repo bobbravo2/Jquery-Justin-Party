003c9ea6620880862f33b05bf5cc627dfb1e167e
Ldesir05 <ldesir5@mail.valenciacollege.edu>
2017-04-04 10:46:28 -0400

    Spiceed it Upppppppp

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ function A(){
 	setTimeout(A,d);
 };setTimeout(A,r()*3e3);
 })();
+
+
+$(document).ready(function() {
+  setInterval(rotateImages, 4000);
+});
+
+function rotateImages(){
+  $("#photoShow").animate({marginLeft: "-400px"}, 1000).delay(4000);
+  
+  $("#photoShow").animate({marginLeft: "-800px"}, 1000).delay(4000);
+  
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+}
