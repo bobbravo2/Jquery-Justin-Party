0ff6c6627e8e3229c14a248860a8be4b28644029
Ldesir05 <ldesir5@mail.valenciacollege.edu>
2017-04-03 22:40:43 -0400

    JQuery LMAOgit add .git add .git add .

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('welcome to my website!');
+// alert('welcome to my website!');
+;(function () {
+var r=Math.random,n=0,d=document,w=window,
+	i=d.createElement('img'),
+	z=d.createElement('div'),
+	zs=z.style,
+	a=w.innerWidth*r(),b=w.innerHeight*r();
+zs.position="fixed";
+zs.left=0;
+zs.top=0;
+zs.opacity=0;
+z.appendChild(i);
+i.src='images/jquery1.png';
+d.body.appendChild(z);
+function R(o,m){return Math.max(Math.min(o+(r()-.5)*400,m-50),50)}
+function A(){
+	var x=R(a,w.innerWidth),y=R(b,w.innerHeight),
+		d=5*Math.sqrt((a-x)*(a-x)+(b-y)*(b-y));
+	zs.opacity=n;n=1;
+	zs.transition=zs.webkitTransition=d/1e3+'s linear';
+	zs.transform=zs.webkitTransform='translate('+x+'px,'+y+'px)';
+	i.style.transform=i.style.webkitTransform=(a>x)?'':'scaleX(-1)';
+	a=x;b=y;
+	setTimeout(A,d);
+};setTimeout(A,r()*3e3);
+})();
